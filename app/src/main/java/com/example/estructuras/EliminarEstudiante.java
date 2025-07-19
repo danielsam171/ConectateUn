c427ac5efad9a5d3466eff5cc9d7b98517415ae7
daniel <dsamacag@unal.edu.co>
2025-07-19 13:15:23 -0500

    Vista completa de AgregarEst,EliminarEst,InformacionEst

--- app/src/main/java/com/example/estructuras/EliminarEstudiante.java
+++ app/src/main/java/com/example/estructuras/EliminarEstudiante.java
@@ -1,6 +1,11 @@
 package com.example.estructuras;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class EliminarEstudiante extends AppCompatActivity {
 
+    EditText campocedula;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,22 @@ public class EliminarEstudiante extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        campocedula = findViewById(R.id.EliCedula);
     }
+    public void eliminarEstudiante(View view){
+        String strcedula = campocedula.getText().toString().trim();
+        if (strcedula.isEmpty()) {
+            campocedula.setError("El correo es obligatorio");
+            return; // Detiene la ejecución si el campo está vacío
+        }
+        long cedula = Long.parseLong(strcedula);
+
+        Intent intent = new Intent(this, MenuPrincipal.class);
+        Toast.makeText(getApplicationContext(), "El estudiante se elimino",
+                Toast.LENGTH_SHORT).show();
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
 }
